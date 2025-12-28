59081823a5e3e21025e642c265adabb16199fb41
Aveniqs <calmus885@gmail.com>
2025-12-28 22:52:36 +0300

    fix: enum fixed, server boots

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -1,10 +1,9 @@
 class Contract < ApplicationRecord
-  enum contract_type: {
+  enum :contract_type, {
     general: 0,
     terms_and_conditions: 1
   }
 
   has_many :clauses, dependent: :destroy
-  has_one_attched :file
+  has_one_attached :file
 end
-
