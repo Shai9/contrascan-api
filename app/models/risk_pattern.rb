ff947c090164aab93ef5689e1c2d6f2a3789c4ed
Aveniqs <calmus885@gmail.com>
2025-12-28 22:12:29 +0300

    feat: generate risk-pattern  model

---
+++ app/models/risk_pattern.rb
@@ -0,0 +1,2 @@
+class RiskPattern < ApplicationRecord
+end
---
+++ test/models/risk_pattern_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class RiskPatternTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
