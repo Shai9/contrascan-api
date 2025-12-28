a3734f3bf25752c772b0a4a1006f7c262f3faa46
Aveniqs <calmus885@gmail.com>
2025-12-28 23:08:47 +0300

    fix: fix upload issue

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -3,22 +3,12 @@ class ContractsController < ApplicationController
     contract = Contract.create!(
       title: params.require(:title),
       file:  params.require(:file),
-      status: "uploaded",
-      raw_text: extract_text(params[:file])
+      status: "uploaded"
     )
-
-    Contracts::DetectContractType.call(contract)
 
     render json: {
       id: contract.id,
-      status: contract.status,
-      contract_type: contract.contract_type
+      status: contract.status
     }, status: :created
-  end
-
-  private
-
-  def extract_text(file)
-    file.read
   end
 end
