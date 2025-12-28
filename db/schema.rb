08dd4b916a0cfeb35e456b27bfd238b3ad2cc380
Aveniqs <calmus885@gmail.com>
2025-12-28 22:22:07 +0300

    feat: run migrations

---
+++ db/schema.rb
@@ -0,0 +1,75 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[8.0].define(version: 2025_12_28_191512) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
+
+  create_table "clauses", force: :cascade do |t|
+    t.bigint "contract_id", null: false
+    t.string "reference"
+    t.text "text"
+    t.integer "position_index"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["contract_id"], name: "index_clauses_on_contract_id"
+  end
+
+  create_table "contracts", force: :cascade do |t|
+    t.string "title"
+    t.integer "contract_type"
+    t.string "status"
+    t.text "raw_text"
+    t.text "normalized_text"
+    t.string "structure"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "risk_matches", force: :cascade do |t|
+    t.bigint "clause_id", null: false
+    t.bigint "risk_pattern_id", null: false
+    t.float "confidence"
+    t.text "explanation"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["clause_id"], name: "index_risk_matches_on_clause_id"
+    t.index ["risk_pattern_id"], name: "index_risk_matches_on_risk_pattern_id"
+  end
+
+  create_table "risk_patterns", force: :cascade do |t|
+    t.string "name"
+    t.string "category"
+    t.string "severity"
+    t.text "keywords"
+    t.integer "applies_to"
+    t.string "ai_label"
+    t.text "user_facing_explanation"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "risk_reports", force: :cascade do |t|
+    t.bigint "contract_id", null: false
+    t.integer "total_risks"
+    t.float "risk_score"
+    t.jsonb "summary"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["contract_id"], name: "index_risk_reports_on_contract_id"
+  end
+
+  add_foreign_key "clauses", "contracts"
+  add_foreign_key "risk_matches", "clauses"
+  add_foreign_key "risk_matches", "risk_patterns"
+  add_foreign_key "risk_reports", "contracts"
+end
