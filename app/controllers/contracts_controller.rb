a1efcb27817716815d064020dd815aa452b4952c
Aveniqs <calmus885@gmail.com>
2025-12-28 22:29:20 +0300

    feat: update contracts cont.

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -1,2 +1,12 @@
 class ContractsController < ApplicationController
+  def create
+    contract = Contract.create!(
+      title: params[:title],
+      file: params[:file],
+      status: "uploaded"
+    )
+
+    render json: { id: contract.id, status: contract.status }, status: :created
+  end
 end
+
