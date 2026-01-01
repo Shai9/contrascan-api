c0843d83d60c99c1347791df02525c96693e2f56
Aveniqs <calmus885@gmail.com>
2026-01-02 01:53:16 +0300

    feat: implement the job

--- app/jobs/process_contract_job.rb
+++ app/jobs/process_contract_job.rb
@@ -1,7 +1,22 @@
 class ProcessContractJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
-    # Do something later
+  def perform(contract_id)
+    contract = Contract.find_by(id: contract_id)
+    return unless contract
+
+    contract.processing!
+
+    Contracts::ExtractText.call(contract)
+    Contracts::DetectContractType.call(contract)
+    Contracts::Extractors::Clauses.call(contract)
+    Contracts::MatchRisks.call(contract)
+    Contracts::GenerateReport.call(contract)
+
+    contract.completed!
+  rescue => e
+    contract&.failed!
+    Rails.logger.error("[ProcessContractJob] #{e.class}: #{e.message}")
+    raise e
   end
 end
